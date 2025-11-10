fa2e02bc669331d94fbc3fdb7ac8b9a3496275a3
Rlimoges <35402321+rlimoges11@users.noreply.github.com>
2025-11-10 12:12:22 -0500

    Chunks - Grid

--- main/main.lua
+++ main/main.lua
@@ -1,23 +1,41 @@
 local JSON = require("lib.vendor.json_min")
 local FLUX = require("lib.vendor.flux_min")
 local SLAB = require("lib.vendor.slab")
-local engine = {}
-local keysPressed={}
-engine.gameData = {}
+local engine = {
+    gameData = {},
+    chunks={},
+    keysPressed={}
+}
 
+function engine.init(args)
+end
 
+function love.update()
+end
+function love.draw()
+    local size = engine.gameData.chunkSize
+    for j = 0, love.graphics.getHeight() / size do
+        for i = 0, love.graphics.getWidth() / size do
+            engine.chunks[i] = {}
+            love.graphics.setColor(1, 1, 1, 0.1)
+            love.graphics.rectangle("line", i * size, j*size, size, size)
+        end
+    end
+end
 function love.load(args)
     engine.gameData = JSON.decode(love.filesystem.read("dat/json/game.json"))
     love.window.setMode(engine.gameData.window.width, engine.gameData.window.height, {vsync = engine.gameData.window.vsync, resizable = engine.gameData.window.resizable})
     love.window.setFullscreen(engine.gameData.window.fullscreen, "desktop")
     engine.font = love.graphics.newFont("dat/fonts/ZenDots-Regular.ttf", 52)
     engine.fHeight = engine.font:getHeight()
+
+    engine.init(args)
 end
 function love.keyreleased(key)
-    keysPressed[key] = nil
+    engine.keysPressed[key] = nil
 end
 function love.keypressed(key)
-    keysPressed[key] = true
+    engine.keysPressed[key] = true
     if key == "q" or key == "escape" then
         love.event.quit()
     end
